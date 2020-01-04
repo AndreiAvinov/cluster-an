6325076fffdda6fd2ebc1092d60e4567487e71a3
Kiriharas <andreiivanchihin.q@ya.ru>
2020-01-04 13:01:31 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -4,30 +4,28 @@ import scipy.cluster.hierarchy as sch
 from sklearn.cluster import KMeans
 from sklearn.cluster import AgglomerativeClustering
 
-
-#import dataframe
+# import dataframe
 df = pandas.read_csv('NewКластеризация.csv', sep=";", index_col="Регион")
 print(df)
 N_COLUMNS = len(df.columns)
 N_ROWS = len(df.index)
 
-#Normalizing data
+# Normalizing dataframe
 for i in range(N_COLUMNS):
-    df.iloc[:, i]=((df.iloc[:, i]-df.iloc[:, i].min())/(df.iloc[:, i].max()-df.iloc[:, i].min()))
+    df.iloc[:, i] = ((df.iloc[:, i] - df.iloc[:, i].min()) / (df.iloc[:, i].max() - df.iloc[:, i].min()))
 
-#KMeans simple
+# KMeans simple
 kmeans = KMeans(n_clusters=3).fit(df)
 centroids = kmeans.cluster_centers_
 
-#Hierarchy
+# Hierarchy
 dendrogram = sch.dendrogram(sch.linkage(df, method='ward'), labels=df.index)
-ac= AgglomerativeClustering(n_clusters=3).fit(df)
+ac = AgglomerativeClustering(n_clusters=3).fit(df)
 
-#pl = plt.scatter(df.iloc[:, 3], df["Количество преступлений на 100000 человек"], c= kmeans.labels_.astype(float), s=50, alpha=0.5)
-
-#pl1 = df.plot.scatter(x="Количество преступлений на 100000 человек", y="Общая площадь жилых помещений")
-#pl2 = df.plot.scatter(x="Среднемесячный размер социальной поддержки на одного пользователя", y="Общая площадь жилых помещений")
-#pl3 = df.plot.scatter(x="Среднемесячный размер социальной поддержки на одного пользователя", y="Количество преступлений на 100000 человек")
+# pl = plt.scatter(df.iloc[:, 3], df["Количество преступлений на 100000 человек"], c= kmeans.labels_.astype(float), s=50, alpha=0.5)
+# pl1 = df.plot.scatter(x="Количество преступлений на 100000 человек", y="Общая площадь жилых помещений")
+# pl2 = df.plot.scatter(x="Среднемесячный размер социальной поддержки на одного пользователя", y="Общая площадь жилых помещений")
+# pl3 = df.plot.scatter(x="Среднемесячный размер социальной поддержки на одного пользователя", y="Количество преступлений на 100000 человек")
 
 df["KMeans"] = kmeans.labels_
 df["AC"] = ac.labels_
@@ -37,5 +35,3 @@ with pandas.option_context('display.max_rows', None, 'display.max_columns', None
 plt.show()
 
 df.to_csv(path_or_buf="res.csv")
-
-
